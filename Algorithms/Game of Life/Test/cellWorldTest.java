779268c86f50b9e767050cc410e381a3a8038985
Mike_Zhang <zhangwengyu999@gmail.com>
2022-08-06 10:27:33 +0800

    update Game of Life

--- Algorithms/Game of Life/Test/cellWorldTest.java
+++ Algorithms/Game of Life/Test/cellWorldTest.java
@@ -1,21 +1,23 @@
+// Made by Mike_Zhang(mike.ultrafish.cn)
+// UltraFish.cn
+// 2022-08-05
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-public class cellWorldTest {
+public class CellWorldTest {
 
     @Test
-    public void lifeTest1() {
-        cellWorld newWorld = new cellWorld(3,3);
-        cell c1 = new cell(0,1);
+    // Blinker
+    public void BlinkerTest() {
+        CellWorld newWorld = new CellWorld(3,3);
+        Cell c1 = new Cell(0,1);
         newWorld.add(c1);
-        cell c2 = new cell(1,1);
+        Cell c2 = new Cell(1,1);
         newWorld.add(c2);
-        cell c3 = new cell(2,1);
+        Cell c3 = new Cell(2,1);
         newWorld.add(c3);
         while (true) {
             try{
-                newWorld.listAll();
+                System.out.println(newWorld.listAll());
                 System.out.println("");
                 newWorld.refresh();
                 Thread.sleep(1000);
@@ -25,41 +27,19 @@ public class cellWorldTest {
     }
 
     @Test
-    public void lifeTest2() {
-        cellWorld newWorld = new cellWorld(4,4);
-        cell c1 = new cell(1,1);
-        cell c2 = new cell(1,2);
-        cell c3 = new cell(2,1);
-        cell c4 = new cell(2,2);
-        newWorld.add(c1);
-        newWorld.add(c2);
-        newWorld.add(c3);
-        newWorld.add(c4);
-        while (true) {
-            try{
-                newWorld.listAll();
-                System.out.println("");
-                newWorld.refresh();
-                Thread.sleep(1000);
-            }
-            catch (Exception e) {}
-        }
-    }
-
-    @Test
-    public void lifeTest3() {
-        cellWorld newWorld = new cellWorld(5,5);
-        cell c1 = new cell(2,1);
-        cell c2 = new cell(1,2);
-        cell c3 = new cell(3,2);
-        cell c4 = new cell(2,3);
+    public void BlockTest() {
+        CellWorld newWorld = new CellWorld(4,4);
+        Cell c1 = new Cell(1,1);
+        Cell c2 = new Cell(1,2);
+        Cell c3 = new Cell(2,1);
+        Cell c4 = new Cell(2,2);
         newWorld.add(c1);
         newWorld.add(c2);
         newWorld.add(c3);
         newWorld.add(c4);
         while (true) {
             try{
-                newWorld.listAll();
+                System.out.println(newWorld.listAll());
                 System.out.println("");
                 newWorld.refresh();
                 Thread.sleep(1000);
@@ -69,13 +49,35 @@ public class cellWorldTest {
     }
 
     @Test
-    public void lifeTest4() {
-        cellWorld newWorld = new cellWorld(15,15);
-        cell c1 = new cell(2,0);
-        cell c2 = new cell(0,1);
-        cell c3 = new cell(2,1);
-        cell c4 = new cell(1,2);
-        cell c5 = new cell(2,2);
+    public void TubTest() {
+        CellWorld newWorld = new CellWorld(5,5);
+        Cell c1 = new Cell(2,1);
+        Cell c2 = new Cell(1,2);
+        Cell c3 = new Cell(3,2);
+        Cell c4 = new Cell(2,3);
+        newWorld.add(c1);
+        newWorld.add(c2);
+        newWorld.add(c3);
+        newWorld.add(c4);
+        while (true) {
+            try{
+                System.out.println(newWorld.listAll());
+                System.out.println("");
+                newWorld.refresh();
+                Thread.sleep(1000);
+            }
+            catch (Exception e) {}
+        }
+    }
+
+    @Test
+    public void GliderTest() {
+        CellWorld newWorld = new CellWorld(15,15);
+        Cell c1 = new Cell(2,0);
+        Cell c2 = new Cell(0,1);
+        Cell c3 = new Cell(2,1);
+        Cell c4 = new Cell(1,2);
+        Cell c5 = new Cell(2,2);
         newWorld.add(c1);
         newWorld.add(c2);
         newWorld.add(c3);
@@ -83,13 +85,15 @@ public class cellWorldTest {
         newWorld.add(c5);
         while (true) {
             try{
-                newWorld.listAll();
+                System.out.println(newWorld.listAll());
                 System.out.println("");
                 newWorld.refresh();
-                Thread.sleep(1000);
+                Thread.sleep(500);
             }
             catch (Exception e) {}
         }
     }
-
 }
+// Made by Mike_Zhang(mike.ultrafish.cn)
+// UltraFish.cn
+// 2022-08-05
